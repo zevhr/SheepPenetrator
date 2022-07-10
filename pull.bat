9784629c139ca77f6a2108cdad4e45dd473e3ae1
Alex <hi@thatalex.dev>
2022-07-10 19:10:55 -0400

    add downloads for cloaketh

--- pull.bat
+++ pull.bat
@@ -3,8 +3,11 @@ mkdir spigot
 
 cd "./spigot"
 mkdir plugins
+mkdir "./plugins/WorldEdit/schematics"
 
 powershell -Command "Invoke-WebRequest https://download.getbukkit.org/spigot/spigot-1.18.1.jar -OutFile ./spigot.jar"
+powershell -Command "Invoke-WebRequest https://github.com/zevhr/SheepPenetrator/releases/download/new/sheep-1.0.jar -OutFile ./plugins/sheep.jar"
+powershell -Command "Invoke-WebRequest https://github.com/zevhr/SheepPenetrator/releases/download/new/sheep3.schem -OutFile ./plugins/WorldEdit/schematics/sheep3.schem"
 powershell -Command "Invoke-WebRequest https://dev.bukkit.org/projects/worldedit/files/latest -OutFile ./plugins/WorldEdit.jar"
 
 echo "Starting a server now. You'll have to edit eula.txt to accept Mojang's EULA."
